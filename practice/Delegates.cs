8d336f044dd0ae4a630cc8913546c87b76e90e94
nichecodes <aneesh.malhotra95@gmail.com>
2023-08-08 22:47:52 -0400

    First commit

---
+++ practice/Delegates.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Delegates
+{
+
+    delegate void MessagePrinter();
+
+    public class DelegateTest
+	{
+		public static void PrintHelloWorld()
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public static void PrintGoodBye()
+		{
+			Console.WriteLine("Good Bye");
+
+		}
+	}
+}
+
---
+++ practice/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Delegates;
+namespace practice
+{
+
+	public class Program
+	{
+        static void Main(string[] args)
+        { 
+            MessagePrinter printer = new MessagePrinter(DelegateTest.PrintHelloWorld);
+
+            MessagePrinter printer2 = DelegateTest.PrintHelloWorld;
+
+            printer += DelegateTest.PrintGoodBye;
+            printer2 += DelegateTest.PrintGoodBye;
+
+            printer();
+            printer2.Invoke();
+
+        }
+    }
+}
+
